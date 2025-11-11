f2c040f1e716705bc5fb577734a9524d391a259f
Gooburr <130682584+Gooburr@users.noreply.github.com>
2025-11-11 00:36:42 -0500

    added music player, finished collision audio, made pins bouncy

--- Assets/Scripts/audio/CollisionAudio.cs
+++ Assets/Scripts/audio/CollisionAudio.cs
@@ -4,27 +4,28 @@ public class CollisionAudio : MonoBehaviour
 {
     private AudioSource AudioSource;
     private Rigidbody rb;
-    private AudioListener Listener;
     public AudioClip clip;
     public float pitchRandomness;
     public float basePitch;
     public float volumeMultiplier;
+    public float sqrMagnitudeSoundMin;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Listener = FindFirstObjectByType<AudioListener>();
         rb = GetComponent<Rigidbody>();
         AudioSource = GetComponent<AudioSource>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
     private void OnCollisionEnter(Collision collision)
     {
         AudioSource.pitch = basePitch + Random.Range(-pitchRandomness, pitchRandomness);
         AudioSource.PlayOneShot(clip, collision.relativeVelocity.magnitude * volumeMultiplier);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.relativeVelocity.sqrMagnitude >= sqrMagnitudeSoundMin)
+        {
+            AudioSource.pitch = basePitch + Random.Range(-pitchRandomness, pitchRandomness);
+            AudioSource.PlayOneShot(clip, collision.relativeVelocity.magnitude * volumeMultiplier);
+        }
     }
 }
